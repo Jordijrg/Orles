47fcd8eb80280b0223ab32bfd5d51b5c849b9ec2
Adria Moya Duran <adriamoyaduran787@gmail.com>
2023-11-20 19:01:53 +0100

    d

--- emeset-app/App/Views/panelporfe.php
+++ emeset-app/App/Views/panelporfe.php
@@ -1,10 +1,27 @@
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>panel profe</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="main.css">
+
+  <title>Todo APP</title>
 </head>
+
 <body>
-    
+
+<div class="grid grid-cols-7 gap-1">
+  <div class="col-span-1" style="border:solid;">01</div>
+  <!-- ... -->
+  <div class="col-span-6" style="border:solid;">09</div>
+</div>
+<script src="js/flowbite.js"></script>
+
+
 </body>
+
 </html>
