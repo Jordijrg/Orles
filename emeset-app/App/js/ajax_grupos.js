305336bdb518d287554888dbfe81d64d4ad7af22
Adria Moya Duran <adriamoyaduran787@gmail.com>
2023-11-27 19:24:35 +0100

    filtros con desplegable de usuarios y modal funcional

--- emeset-app/App/js/ajax_grupos.js
+++ emeset-app/App/js/ajax_grupos.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
 
+// initialize components based on data attribute selectors
 function grupoajax(){
+    initFlowbite();
    
     let control_witedmax=false
     $("#grupo_search").on("keydown",(evt)=>{
@@ -18,28 +20,90 @@ function grupoajax(){
                 data: {
                     texto:value_input
                 },
-                async:false
-                ,
+              
                  // data recive un objeto con la informacion que se enviara al servidor
                 success: function(datos) {
                     //success es una funcion que se utiliza si el servidor retorna informacion
                     $(".valores_grupos").html(``)
                  
                     let grupos=datos.result
-                    grupos.forEach(element => {
+                    $("#valores_grupos").append(`
+                            <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+                            <link rel="stylesheet" href="/resources/demos/style.css">
+                            <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+                            <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+                            <script>
+                            $( function() {
+                              $( "#accordion" ).accordion({
+                                collapsible: true
+                              });
+                            } );
 
-                        $(".valores_grupos").append(`
-                        <div class="border-b-2 border-b-indigo-400 mt-10 flex flex-row	justify-between	">
-                        <p>${element.Nom}</p>
-                        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-                        <span class="relative transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 px-7 p-1">
-                            Visualizar
-                        </span>
-                        </button>
-                    </div>
-                        
-                        `)
-                    });
+
+// initialize components based on data attribute selectors
+
+
+                            </script>
+   
+
+                            `)
+
+                            grupos.forEach((element,i) => {
+                   
+                      
+                                
+                                $(".valores_grupos").append(`
+
+
+                                <h3>${element.Nom}</h3>
+                                                              `)
+
+                                        $.ajax({
+                                        type: "POST", // la variable type guarda el tipo de la peticion GET,POST,..
+                                        url: "/alumngrupajax", //url guarda la ruta hacia donde se hace la peticion
+                                        data: {
+                                            idgrupo:element.IdGrup
+                                        },
+                                        async:false
+                                        , // data recive un objeto con la informacion que se enviara al servidor
+                                        success: function(datos) {
+
+                                            //success es una funcion que se utiliza si el servidor retorna informacion
+                                            let usuarios=datos.result
+                                            if(datos==null){
+                                            }   else{
+                                                usuarios.forEach(element=>{
+                                                    $(".valores_grupos").append(`
+
+                                                    <div >
+                                                    <button class="modales" data-modal-target="default-modal" data-modal-toggle="default-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                                                    ${element.Nom}
+</button>
+                                                    </div></div>
+    `)
+                                                })
+                                                jQuery(".modales").on("click",()=>{
+                                                    initFlowbite();
+                                                    const modal = FlowbiteInstances.getInstance('Modal', 'default-modal');
+                                                    modal.show();
+                                                })
+                                               
+                                            }
+                                           
+                                         
+                                            
+                                        },
+                            
+                                        dataType: "json", // El tipo de datos esperados del servidor. Valor predeterminado: Intelligent Guess (xml, json, script, text, html).
+                                    });
+                            
+                            /*
+                             <div>
+                                  <p id=>Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.</p>
+                                </div>
+                            */
+                        });
+                     
                  
                 },
     
@@ -55,8 +119,7 @@ function grupoajax(){
                         data: {
                             texto:value_input
                         },
-                        async:false
-                        , // data recive un objeto con la informacion que se enviara al servidor
+                      // data recive un objeto con la informacion que se enviara al servidor
                         success: function(datos) {
                             //success es una funcion que se utiliza si el servidor retorna informacion
                             $(".valores_grupos").html(``)
@@ -83,7 +146,7 @@ function grupoajax(){
                                     $(".valores_grupos").append(`
 
  
-                                    <h3>${element.Nom}</h3>
+                                    <h3 >${element.Nom}</h3>
                                                                   `)
 
                                             $.ajax({
@@ -97,20 +160,22 @@ function grupoajax(){
                                             success: function(datos) {
                                                 //success es una funcion que se utiliza si el servidor retorna informacion
                                                 let usuarios=datos.result
-                                                console.log(datos)
                                                 if(datos==null){
-                                                 console.log()
                                                 }   else{
                                                     usuarios.forEach(element=>{
                                                         $(".valores_grupos").append(`
 
-                                                        <div>
-                                                          <p>${element.Nom}</p>
+                                                        <div data-modal-target="default-modal" data-modal-toggle="default-modal" class=" modales">
+                                                          <p >${element.Nom}</p>
                                                         </div></div>
         `)
                                                     })
                                                  
-                                                   
+                                                    jQuery(".modales").on("click",()=>{
+                                                        initFlowbite();
+                                                        const modal = FlowbiteInstances.getInstance('Modal', 'default-modal');
+                                                        modal.show();
+                                                    })
                                                 }
                                                
                                              
@@ -134,6 +199,9 @@ function grupoajax(){
                 }
     
         }
+      
+    
     })
+
 }
 export {grupoajax}; 
