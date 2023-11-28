6f8a85c9d19273ea55f5ca71bbc6fe56e79aa36b
Jordi <jrodriguez4@cendrassos.net>
2023-11-28 17:15:10 +0100

    dark

--- emeset-app/App/js/darkmode.js
+++ emeset-app/App/js/darkmode.js
@@ -3,27 +3,29 @@ function toggleDarkMode() {
   const htmlElement = document.getElementById('html');
   const isDarkModeEnabled = htmlElement.classList.toggle('dark');
 
-  // Actualizar el estado del toggle
-  const toggleDarkModeButton = document.getElementById('toggleDarkModeButton');
-  toggleDarkModeButton.checked = isDarkModeEnabled;
-
   // Guardar la preferencia en localStorage
   localStorage.setItem('darkMode', isDarkModeEnabled ? 'enabled' : 'disabled');
 }
 
-// Agregar evento de clic al botón para alternar y guardar el modo oscuro
+// Agregar evento de cambio al botón para alternar y guardar el modo oscuro
 const toggleDarkModeButton = document.getElementById('toggleDarkModeButton');
-toggleDarkModeButton.addEventListener('click', toggleDarkMode);
+toggleDarkModeButton.addEventListener('change', toggleDarkMode);
 
 // Verificar y aplicar la preferencia almacenada en localStorage al cargar la página
-window.addEventListener('load', function() {
+document.addEventListener('DOMContentLoaded', function() {
   const htmlElement = document.getElementById('html');
   const storedDarkMode = localStorage.getItem('darkMode');
 
-  // Si la preferencia está almacenada, aplicarla y actualizar el estado del toggle
+  // Si la preferencia está almacenada y es 'enabled', aplicarla
   if (storedDarkMode === 'enabled') {
     htmlElement.classList.add('dark');
     toggleDarkModeButton.checked = true;
+  } else {
+    htmlElement.classList.remove('dark');
+    toggleDarkModeButton.checked = false;
   }
 });
+
+
+
 export {toggleDarkMode};
