cc228d52aada02ffc6ec753ea81039a7efdf2bea
Adria Jofre Cantón <ajofre@cendrasso.net>
2023-11-29 16:53:19 +0100

    a

--- emeset-app/public/index.php
+++ emeset-app/public/index.php
@@ -29,8 +29,8 @@ use App\Controllers\profilecontroller;
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 include "../vendor/autoload.php";
 
-/* Creem els diferents models */
-$contenidor = new \App\Container(__DIR__ . "/../App/config.php");
+// Creem els diferents models 
+$contenidor = new \App\Container(__DIR__ . "/../App/config.php"); 
 
 $app = new \Emeset\Emeset($contenidor);
 
@@ -51,13 +51,6 @@ $app->get("/perfil", [profilecontroller::class,"index"]);
 
 
 
-$app->get("/login", "\App\Controllers\LoginController:index");
-$app->post("/login", "\App\Controllers\LoginController:login");
-$app->get("/resnav", "\App\Controllers\LoginController:resnav");
-$app->get("/logout", "\App\Controllers\LoginController:logout", [[\App\Middleware\Auth::class,"auth"]]);
-
-
-
 $app->get("/adminpanel", [adminpanelcontroller::class,"index"] /*, [[\App\Middleware\Auth::class,"auth"]] */);
 $app->get("/deleteuser/{id}", [adminpanelcontroller::class,"deleteuser"]);
 $app->post("/adduser", [adminpanelcontroller::class,"adduser"]);
@@ -74,11 +67,9 @@ $app->post("/openModal", [adminpanelcontroller::class, "updateModal"]);
 
 $app->get("/register", [registercontroller::class,"addregister"]);
 
+$app->get("/login", [registercontroller::class,"login"]);
+$app->post("/login", [registercontroller::class,"dologin"]);
 
-$app->get("/login", "\App\Controllers\LoginController:index");
-$app->post("/login", "\App\Controllers\LoginController:login");
-$app->get("/resnav", "\App\Controllers\LoginController:resnav");
-$app->get("/logout", "\App\Controllers\LoginController:logout", [[\App\Middleware\Auth::class,"auth"]]);
 
 $app->route(Router::DEFAULT_ROUTE, "\App\Controllers\ErrorController:error404");
 
