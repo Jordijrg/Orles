7dec4c003b1349b67bafefadd385a781974ee7cb
Adria Moya Duran <adriamoyaduran787@gmail.com>
2023-12-01 15:28:20 +0100

    modals_bugs

--- emeset-app/App/Views/panelporfe.php
+++ emeset-app/App/Views/panelporfe.php
@@ -11,9 +11,7 @@
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
 <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="/resources/demos/style.css">
   <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="/resources/demos/style.css">
   <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
   <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
  <script>
@@ -24,13 +22,12 @@
                                   } );
  </script>
 <?php include "script_icons.php" ?>
-<style>
 
-</style>
   <title>Todo APP</title>
 </head>
 
 <body>
+  
   <?php include "header.php";  ?>
 <section class="bg-indigo-50 h-screen">
 
@@ -57,10 +54,8 @@
         </form>
         <div class="grupos " id="valores_grupos">
         <div id="accordion" class="valores_grupos">
-                               
-        </div>
 
-  
+                                </div>
  
           </div>  
       </div>
