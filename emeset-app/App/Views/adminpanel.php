27fd05cffdc3b8a508aaec9052eb31d1707d746f
Adria Jofre Cantón <ajofre@cendrasso.net>
2023-12-19 12:50:17 +0100

    final

--- emeset-app/App/Views/adminpanel.php
+++ emeset-app/App/Views/adminpanel.php
@@ -338,15 +338,15 @@
 
         <div data-modal-target="static-modal" data-modal-toggle="static-modal" class="max-w-sm m-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <div>
-                <a href=""></a>
+               
             </div>
-            <a href="#">
+            
                 <img class="rounded-t-lg" src="/images/img1.jpg" alt="" />
-            </a>
+        
             <div class="p-5">
-                <a href="#">
+                
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><?php echo $value["nom"]; ?></h5>
-                </a>
+                
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
                     acquisitions of 2021 so far, in reverse chronological order.</p>
             </div>
