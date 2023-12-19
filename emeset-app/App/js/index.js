62cf0cee7fc75c57247cd138e84166e23adf9ba0
Adria Moya Duran <adriamoyaduran787@gmail.com>
2023-12-19 22:09:38 +0100

    d

--- emeset-app/App/js/index.js
+++ emeset-app/App/js/index.js
@@ -13,11 +13,9 @@ import {viwer} from './ajax_btn_viwer.js';
 
 import {upload_div} from './upload_div.js';
 import {form_img} from './show_form_img.js'
-import {delmissatge} from './delmissatge.js';
 import {camera_js} from './camera.js';
 
 
-delmissatge();
 viwer()
 darkmode();
 scrollFunction();
