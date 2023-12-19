3fdcfd3c203310dccc28e9fb88c4b48c9f883905
Jordi <jrodriguez4@cendrassos.net>
2023-12-19 23:11:51 +0100

    hola

--- emeset-app/App/js/index.js
+++ emeset-app/App/js/index.js
@@ -15,14 +15,14 @@ import {upload_div} from './upload_div.js';
 import {form_img} from './show_form_img.js'
 import {camera_js} from './camera.js';
 
-
+delmissatge();
 viwer()
 darkmode();
 scrollFunction();
 scrollToTop();
 addfotoorla();
 randomuser();
-delmissatge();
+
 camera_js();
 
 jQuery("#menu_main").hide()
