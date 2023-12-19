02eb67ba1ceceff8e5bfe63455da2d30c1dd1f68
Adria Moya Duran <adriamoyaduran787@gmail.com>
2023-12-20 00:05:40 +0100

    menu for roles

--- emeset-app/App/Views/header.php
+++ emeset-app/App/Views/header.php
@@ -119,14 +119,21 @@
           <a href="/alumne"
             class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Alumne</a>
         </li>
-        <li>
+        <?php if($user["rol"] === "profe" || $user["rol"] === "equip_directiu" || $user["rol"] === "admin"){?>
+          <li>
           <a href="/panelprofe"
             class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Panell de professor</a>
         </li>
+        <?php } ?>
+        <?php
+            if ($user["rol"] === "equip_directiu" || $user["rol"] === "admin") {?>
         <li>
           <a href="/adminpanel"
             class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Panell d'administrador</a>
         </li>
+        <?php
+            }
+            ?>  
         <li>
           <?php
             if ($user["rol"] === "profe" || $user["rol"] === "equip_directiu" || $user["rol"] === "admin") {?>
